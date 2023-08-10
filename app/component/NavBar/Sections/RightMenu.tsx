8fa22d8e89c37f6e26de4e1c65b44607fcb4bd6e
Ddongh <qwer5383@naver.com>
2023-08-10 23:05:37 +0900

    LeftMenu Update

---
+++ app/component/NavBar/Sections/RightMenu.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LeftMenu: React.FC = () => {
+    return (
+        <p>RightMenu</p>
+    );
+}
+
+export default LeftMenu
---
+++ app/register/page.tsx
@@ -0,0 +1,13 @@
+import React, {useState} from 'react'
+
+// use client
+const RegisterPage: React.FC = () => {
+
+    // const [first, setfirst] = useState(false)
+
+    return (
+        <p>RegisterPage</p>
+    );
+}
+
+export default RegisterPage
